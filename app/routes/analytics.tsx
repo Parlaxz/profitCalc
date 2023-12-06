fff0fb84cd160fd2173dcc72124f122cd4003f8a
Parlaxz <moshimoshiistore@gmail.com>
2023-12-06 14:12:11 -0600

    Updated analytics to make date picker work

--- app/routes/analytics.tsx
+++ app/routes/analytics.tsx
@@ -114,26 +114,26 @@ export const meta = () => {
 
 // ./app/routes/index.tsx
 export default function Index() {
-  var firstDay = new Date(new Date().getFullYear(), 0, 1);
-  // .toISOString().split("T")[0];
+  const currentDate = new Date();
+  const firstDayOfYear = new Date(currentDate.getFullYear(), 0, 1);
+  const startOfYear = new Date(currentDate.getFullYear(), 0, 0);
+  const millisecondsPerDay = 1000 * 60 * 60 * 24;
+  const dayOfYear = Math.floor(
+    (currentDate - startOfYear) / millisecondsPerDay
+  );
 
-  var now = new Date();
-  var start = new Date(now.getFullYear(), 0, 0);
-  var diff = now - start;
-  var oneDay = 1000 * 60 * 60 * 24;
-  var day = Math.floor(diff / oneDay);
-
-  console.log(firstDay);
   const loaderData = useLoaderData<typeof loader>();
   const [pageType, setPageType] = useState("dashboard");
   const [pageData, setPageData] = useState(loaderData);
-  const [startDate, setStartDate] = useState(firstDay);
-  const [endDate, setEndDate] = useState(now);
-
+  const [startDate, setStartDate] = useState(new Date("2023-11-01"));
+  const [endDate, setEndDate] = useState(currentDate);
   useEffect(() => {
     setPageData(loaderData);
   }, [loaderData]);
-  console.log("pageData", pageData[1]);
+  const filteredData = pageData.filter((data) => {
+    const orderDate = new Date(data.date);
+    return orderDate >= startDate && orderDate <= endDate;
+  });
 
   return (
     <div className="max-h-screen h-screen bg-white flex justify-center items-center">
@@ -147,71 +147,77 @@ export default function Index() {
         <div className="grid gap-8 grid-rows-2 w-full h-full p-8">
           <Card>
             <>
-              {" "}
               <DatePicker
                 selected={endDate}
                 onChange={(date) => setEndDate(date)}
-              />{" "}
+              />
               <DatePicker
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
               />
               {pageData.length > 0 && (
-                <MyResponsiveLine dailyAnalytics={pageData} />
+                <MyResponsiveLine dailyAnalytics={filteredData} />
               )}
             </>
           </Card>
           <div className="grid gap-2 grid-cols-3">
-            {" "}
             <Card>
               <>
                 <div>
                   Profit:{" "}
-                  {pageData
-                    .reduce((sum, dataPoint) => {
-                      return sum + parseFloat(dataPoint.data.profit.current);
-                    }, 0)
+                  {filteredData
+                    .reduce(
+                      (sum, dataPoint) =>
+                        sum + parseFloat(dataPoint.data.profit.current),
+                      0
+                    )
                     .toFixed(2)}
                 </div>
                 <div>
                   Revenue:{" "}
-                  {pageData
-                    .reduce((sum, dataPoint) => {
-                      return (
-                        sum + parseFloat(dataPoint.data.shopify.grossRevenue)
-                      );
-                    }, 0)
+                  {filteredData
+                    .reduce(
+                      (sum, dataPoint) =>
+                        sum + parseFloat(dataPoint.data.shopify.grossRevenue),
+                      0
+                    )
                     .toFixed(2)}
                 </div>
                 <div>
                   Printing Cost:{" "}
-                  {pageData
-                    .reduce((sum, dataPoint) => {
-                      return sum + parseFloat(dataPoint.data.printify.cost);
-                    }, 0)
+                  {filteredData
+                    .reduce(
+                      (sum, dataPoint) =>
+                        sum + parseFloat(dataPoint.data.printify.cost),
+                      0
+                    )
                     .toFixed(2)}
                 </div>
                 <div>
                   Ad Spend:{" "}
-                  {pageData
-                    .reduce((sum, dataPoint) => {
-                      return sum + parseFloat(dataPoint.data.meta.currentSpend);
-                    }, 0)
+                  {filteredData
+                    .reduce(
+                      (sum, dataPoint) =>
+                        sum + parseFloat(dataPoint.data.meta.currentSpend),
+                      0
+                    )
                     .toFixed(2)}
                 </div>
                 <div>
                   Expected Cashback:{" "}
-                  {pageData
-                    .reduce((sum, dataPoint) => {
-                      return sum + parseFloat(dataPoint.data.stats.cashback);
-                    }, 0)
+                  {filteredData
+                    .reduce(
+                      (sum, dataPoint) =>
+                        sum + parseFloat(dataPoint.data.stats.cashback),
+                      0
+                    )
                     .toFixed(2)}
                 </div>
               </>
-            </Card>{" "}
+            </Card>
             <Card>
               <></>
-            </Card>{" "}
+            </Card>
             <Card>
               <></>
             </Card>
@@ -281,102 +287,148 @@ const transformDataForLineChart = (dailyAnalyticsData) => {
 const MyResponsiveLine = ({ dailyAnalytics }) => {
   console.log("dailyAnalytics", dailyAnalytics.length);
   const transformedData = transformDataForLineChart(dailyAnalytics);
+  const [showProfit, setShowProfit] = useState(true);
+  const [showRevenue, setShowRevenue] = useState(true);
+  const [showCost, setShowCost] = useState(true);
+  const data = [];
+
+  if (showProfit) {
+    const profitData = transformedData.map((point) => ({
+      x: point.x,
+      y: point.Profit,
+    }));
+    data.push({
+      id: "Profit",
+      color: "hsl(120, 70%, 50%)",
+      data: profitData,
+    });
+  }
+
+  if (showRevenue) {
+    const revenueData = transformedData.map((point) => ({
+      x: point.x,
+      y: point.Revenue,
+    }));
+    data.push({
+      id: "Revenue",
+      color: "hsl(60, 70%, 50%)",
+      data: revenueData,
+    });
+  }
+  if (showCost) {
+    const costData = transformedData.map((point) => ({
+      x: point.x,
+      y: point.Cost,
+    }));
+    data.push({
+      id: "Cost",
+      color: "hsl(0, 70%, 50%)",
+      data: costData,
+    });
+  }
+
   return (
-    <ResponsiveLine
-      data={[
-        {
-          id: "Profit",
-          color: "hsl(120, 70%, 50%)",
-          data: transformedData.map((point) => ({
-            x: point.x,
-            y: point.Profit,
-          })),
-        },
-        // {
-        //   id: "Ad Spend",
-        //   color: "hsl(240, 70%, 50%)",
-        //   data: transformedData.map((point) => ({
-        //     x: point.x,
-        //     y: point["Ad Spend"],
-        //   })),
-        // },
-        {
-          id: "Revenue",
-          color: "hsl(60, 70%, 50%)",
-          data: transformedData.map((point) => ({
-            x: point.x,
-            y: point.Revenue,
-          })),
-        },
-        {
-          id: "Cost",
-          color: "hsl(0, 70%, 50%)",
-          data: transformedData.map((point) => ({ x: point.x, y: point.Cost })),
-        },
-      ]}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: "point" }}
-      yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-        stacked: true,
-        reverse: false,
-      }}
-      yFormat=" >-.2f"
-      curve="natural"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "Date",
-        legendOffset: 36,
-        legendPosition: "middle",
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "amount($)",
-        legendOffset: -40,
-        legendPosition: "middle",
-      }}
-      enableGridX={false}
-      pointSize={3}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: "serieColor" }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
+    <>
+      <div className="flex justify-end">
+        <button
+          className={`${
+            showProfit ? "bg-green-800" : "bg-gray-500"
+          } hover:bg-green-600 text-white font-bold py-2 px-4 rounded`}
+          onClick={() => {
+            setShowProfit(!showProfit);
+          }}
+        >
+          Toggle Profit
+        </button>
+        <button
+          className={`${
+            showRevenue ? "bg-blue-800" : "bg-gray-500"
+          } hover:bg-blue-600 text-white font-bold py-2 px-4 mx-2  rounded`}
+          onClick={() => {
+            setShowRevenue(!showRevenue);
+          }}
+        >
+          Toggle Revenue
+        </button>
+        <button
+          className={`${
+            showCost ? "bg-red-800" : "bg-gray-500"
+          } hover:bg-red-600 text-white font-bold py-2 px-4 rounded`}
+          onClick={() => {
+            setShowCost(!showCost);
+          }}
+        >
+          Toggle Cost
+        </button>
+      </div>
+      {/* Rest of the code */}
+
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        curve="natural"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "Date",
+          legendOffset: 36,
+          legendPosition: "middle",
+        }}
+        axisLeft={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "amount($)",
+          legendOffset: -40,
+          legendPosition: "middle",
+        }}
+        enableGridX={false}
+        pointSize={3}
+        pointColor={{ theme: "background" }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "serieColor" }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1,
+                },
               },
-            },
-          ],
-        },
-      ]}
-      // ... other props remain unchanged
-    />
+            ],
+          },
+        ]}
+        // ... other props remain unchanged
+      />
+    </>
   );
 };
