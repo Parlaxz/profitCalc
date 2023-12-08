0088a3cfaabcca1a91d5b736cef431eb25fb716f
Parlaxz <moshimoshiistore@gmail.com>
2023-12-08 01:22:19 -0600

    test3

--- app/routes/api.createUserEvent.tsx
+++ app/routes/api.createUserEvent.tsx
@@ -2,6 +2,7 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 
 export async function action({ request }: ActionFunctionArgs) {
   console.log(request);
+  console.log("request.body", request.body);
 
   return { status: 200, body: "ok" };
 }
