c41b0528f3a433b8ff221eb25d9e31238a475c15
Parlaxz <moshimoshiistore@gmail.com>
2023-12-11 15:40:25 -0600

    orderWebhook

---
+++ app/routes/api.orderUpdate.tsx
@@ -0,0 +1,6 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const payload = await request.json();
+  console.log("payload", payload);
+}
