f2f947f256319bed29678bfa0feb76233323d0db
Parlaxz <moshimoshiistore@gmail.com>
2023-12-11 15:41:56 -0600

    updat2

--- app/routes/api.orderUpdate.tsx
+++ app/routes/api.orderUpdate.tsx
@@ -3,4 +3,5 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 export async function action({ request }: ActionFunctionArgs) {
   const payload = await request.json();
   console.log("payload", payload);
+  return { status: 200, body: "ok" };
 }
