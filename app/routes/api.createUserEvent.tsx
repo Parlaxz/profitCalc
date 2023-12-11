987d83a95fbc2780e5d4ba2d5a056153b0ea9bad
Parlaxz <moshimoshiistore@gmail.com>
2023-12-10 21:58:06 -0600

    fix error 2

--- app/routes/api.createUserEvent.tsx
+++ app/routes/api.createUserEvent.tsx
@@ -1,8 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 
 export async function action({ request }: ActionFunctionArgs) {
-  console.log(request);
-  console.log("request.json()", await request.json());
   const payload = await request.json();
   console.log("payload", payload);
   return { status: 200, body: "ok" };
