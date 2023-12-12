73bdaf27dc4351d6d1d592c15fa40cbaccb8275b
Parlaxz <moshimoshiistore@gmail.com>
2023-12-11 18:42:15 -0600

    updates  user with order

--- app/routes/api.orderUpdate.tsx
+++ app/routes/api.orderUpdate.tsx
@@ -20,9 +20,63 @@ export async function action({ request }: ActionFunctionArgs) {
     }),
   };
 
-  const prismaOrder = await prisma.shopifyOrder.create({
-    data: order,
-  });
+  try {
+    const prismaOrder = await prisma.shopifyOrder.create({
+      data: order,
+    });
+  } catch (err) {
+    console.log("shopify Order creation failed with error:", err);
+  }
+
+  try {
+    const getUser = await prisma.user.findFirst({
+      where: {
+        ip: order.ip,
+      },
+    });
+    let arr = [
+      {
+        type: "purchase",
+        timeCreated: new Date().toISOString(),
+        lines: order.lineItems,
+        value: order.revenue,
+      },
+    ];
+    if (getUser) {
+      arr = [...getUser.events, ...arr];
+    }
+
+    const upsertUser = await prisma.user.upsert({
+      where: {
+        ip: order.ip,
+      },
+      update: {
+        timeUpdated: new Date().toISOString(),
+        events: arr,
+      },
+      create: {
+        UTM: {
+          utmSource: "",
+          utmMedium: "",
+          utmCampaign: "",
+          valid: false,
+        },
+        timeCreated: new Date().toISOString(),
+        timeUpdated: new Date().toISOString(),
+        ip: order.ip,
+        events: [
+          {
+            type: "purchase",
+            timeCreated: new Date().toISOString(),
+            lines: order.lineItems,
+            value: order.revenue,
+          },
+        ],
+      },
+    });
+  } catch (err) {
+    console.log("user creation / updating with order failed with err:", err);
+  }
   console.log("prismaOrder", prismaOrder);
   return { status: 200, body: order };
 }
